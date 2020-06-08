facfb2d0465821c3b7f365d5a9d81bc78eab7cfa
Peter <jp-pacinos@users.noreply.github.com>
2020-06-08 21:17:26 +0800

    Update ColHeader.jsx
    removed unused React.forwardRef function

--- src/components/ColHeader.jsx
+++ src/components/ColHeader.jsx
@@ -1,17 +1,12 @@
-import React, { forwardRef } from 'react'
+import React from 'react'
 
-const ColHeader = forwardRef(
-  ({ title, color = 'blue', renderActions }, ref) => (
-    <div
-      ref={ref}
-      className={`flex items-center border-${color}-400 border-b-4 pb-4 mb-4`}
-    >
-      <h2 className="flex-1 w-100 font-semibold text-gray-700 uppercase">
-        {title}
-      </h2>
-      {renderActions}
-    </div>
-  )
+const ColHeader = ({ title, color = 'blue', renderActions }) => (
+  <div className={`flex items-center border-${color}-400 border-b-4 pb-4 mb-4`}>
+    <h2 className="flex-1 w-100 font-semibold text-gray-700 uppercase">
+      {title}
+    </h2>
+    {renderActions}
+  </div>
 )
 
 export default React.memo(ColHeader)
